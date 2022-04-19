2bfa3f93475c5967b84a2ad50f11de665b9f5935
KyaraDeWinter <70946292+KyaraDeWinter@users.noreply.github.com>
2022-04-19 13:05:50 +0200

    router

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './Card.css';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
--- src/pages/News/News.js
+++ src/pages/News/News.js
@@ -4,25 +4,17 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Navbar from './components/Navbar/Navbar.js';
-import Banner from './components/Banner/Banner.js';
 import Card from './components/Card/Card.js';
-import Premium from './components/Premium/Premium.js';
-import About from './components/About/About.js'
-import Contact from './components/Contact/Contact.js'
 import Footer from './components/Footer/Footer.js';
 
-function App() {
+function News() {
   return (
     <>
       <Navbar/>
-      <Banner/>
       <Card/>
-      <Premium/>
-      <About/>
-      <Contact/>
       <Footer/>
     </>
   );
 }
 
-export default App;
+export default News;
